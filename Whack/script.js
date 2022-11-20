05f3f051b666b90a90d58eb2ae18071e0657cac8
Barni Balázs <barni20050909@gmail.com>
2022-11-20 20:13:19 +0100

    szamlalo

--- Whack/script.js
+++ Whack/script.js
@@ -4,43 +4,52 @@ const timeLeftelement = document.querySelector('#time-left');
 const score = document.querySelector('#pont');
 var i = 0;
 let result = 0;
-let timeLeft = 60;
+let timeLeft = 10;
 let interval = -1;
+let running = false;
 start.addEventListener('click', StartGame);
 
 function StartGame(e) {
-    result=0;
-    score.innerHTML=result;
-    timeLeft=60;
-    timeLeftelement.innerHTML=timeLeft;
-    if(interval!=-1)
-    {
+    running = true;
+    result = 0;
+    score.innerHTML = result;
+    timeLeft = 10;
+    timeLeftelement.innerHTML = timeLeft;
+    if (interval != -1) {
         clearInterval(interval);
     }
     buttons.forEach(element => {
         element.classList.remove('pop');
         element.removeEventListener('click', ClickTile);
     });
-    i = Math.floor(25*Math.random());
+    i = Math.floor(25 * Math.random());
     buttons[i].addEventListener('click', ClickTile);
     buttons[i].classList.add('pop');
-    interval=setInterval(()=>{
+
+
+    interval = setInterval(() => {
+
         timeLeft--;
-        timeLeftelement.innerHTML=timeLeft;
-        
-    },1000)
+        timeLeftelement.innerHTML = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+            running = false;
+        }
+    }, 1000)
+
+
 }
 
 function ClickTile(e) {
+    if (!running) {
+        return;
+    }
     e.target.removeEventListener('click', ClickTile);
     e.target.classList.remove('pop');
-    i = Math.floor(25*Math.random());
+    i = Math.floor(25 * Math.random());
     buttons[i].addEventListener('click', ClickTile);
     buttons[i].classList.add('pop');
     result++;
-    score.innerHTML=result;
-}
+    score.innerHTML = result;
 
-function countDown() {
-    
 }
